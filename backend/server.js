35a93398e808899fd7999c6789bef488958c0070
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-03-08 22:30:00 +0530

    pin models and routes

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
-
 dotenv.config();
+
+// Import Routes
+const pinRoutes = require('./routes/pins')
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+// Route Middlewares
+app.use('/api/pins', pinRoutes);
+
 // Connect to MongoDB
 mongoose
 .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
