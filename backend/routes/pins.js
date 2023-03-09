ed1fb30586b9bd550a7b10154e66e2def853b477
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-03-09 23:26:08 +0530

    pin routes

--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -13,11 +13,13 @@ router.post('/', async (req, res) => {
 });
 
 // Get all Pins
-
-// Get a specific Pin
-
-// Delete a Pin
-
-// Update a Pin
+router.get('/', async (req, res) => {
+    try {
+        const pins = await Pin.find();
+        res.status(200).json(pins);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
